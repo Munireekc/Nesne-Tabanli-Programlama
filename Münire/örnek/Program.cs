8a6ba322bef489d978a21aa115f6b5d299d96b6c
Munireekc <94890181+Munireekc@users.noreply.github.com>
2021-12-13 20:45:18 +0300

    Add files via upload

---
+++ Münire/örnek/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyApplication
+{
+    interface AdSoyad
+    {
+        void AdSoyad();
+
+
+        class İsim : AdSoyad
+        {
+            public void AdSoyad()
+            {
+                string isim;
+                Console.WriteLine("Adını Gir:");
+                isim = Console.ReadLine();
+                Console.WriteLine("Merhaba " + " " + isim);
+                Console.WriteLine("Devam Etmek İçin 'Enter' tuşuna basınız");
+                Console.ReadKey();
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string malzeme;
+                string yemekAdi;
+
+                İsim Ad = new İsim();
+                Ad.AdSoyad();
+
+                
+                Console.WriteLine("Yemeğin Adını Giriniz :");
+                yemekAdi = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("lütfen malzemeleri yazarken araya virgül koyunuz");
+                Console.WriteLine();
+
+                Console.WriteLine("Malzemeleri Giriniz :");
+                malzeme = Console.ReadLine();
+
+
+                Console.WriteLine("Yapılacak Yemeğin Adı: " + yemekAdi);
+                Console.WriteLine("Yemek İçin Gerekli malzemeler: " + malzeme);
+                Console.ReadLine();
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
